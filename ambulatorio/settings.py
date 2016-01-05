357e951e253efbeea4c1546b2e60616b640551fe
Fabio Perfetti <perfabio87@gmail.com>
2016-01-05 18:30:28 +0100

    Add CORS; comment permissions; add QuerySet

--- ambulatorio/settings.py
+++ ambulatorio/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_countries',
     'django_filters',
     'crispy_forms',
+    'corsheaders',
 
     'anagrafica',
 ]
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,3 +132,5 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
--- anagrafica/query.py
+++ anagrafica/query.py
@@ -10,16 +10,17 @@ class PazientiQuerySet(models.QuerySet):
             paziente = self.get(pk=paziente_id)
         except ObjectDoesNotExist:
             raise Http404
-        print(paziente)
         return paziente.visite
 
-class VisiteQuerySet(models.QuerySet):
-    def perPaziente(self, paziente_id):
-        try:
-            paziente = self.get(pk=paziente_id)
-        except ObjectDoesNotExist:
-            raise Http404
-        return self.filter(paziente_id=paziente_id).all()
 
-    def perPazienteVisita(self, paziente_id, visita_id):
-        return self.filter(id=visita_id, paziente_id=paziente_id )
+class VisiteQuerySet(models.QuerySet):
+    pass
+    # def perPaziente(self, paziente_id):
+    #     try:
+    #         paziente = self.get(paziente_id=paziente_id)
+    #     except ObjectDoesNotExist:
+    #         raise Http404
+    #     return self.filter(paziente_id=paziente.id).all()
+    #
+    # def perPazienteVisita(self, paziente_id, visita_id):
+    #     return self.filter(id=visita_id, paziente_id=paziente_id )
